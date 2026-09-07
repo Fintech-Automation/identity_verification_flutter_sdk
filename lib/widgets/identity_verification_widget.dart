2322d6f8c6d4ac119a39b85ca060fc0002fcab2f
Yousheng Zhang <yzhang@fintechautomation.com>
2026-09-07 22:57:14 +0800

    update

--- lib/widgets/identity_verification_widget.dart
+++ lib/widgets/identity_verification_widget.dart
@@ -95,9 +95,9 @@ class _IdentityVerificationWidgetState
   @override
   Widget build(BuildContext context) {
     return InAppWebView(
-      initialUrlRequest: URLRequest(url: WebUri('https://192.168.31.17:5173/')),
-      // initialFile:
-      //     'packages/face_liveness_flutter_sdk/assets/html/face_liveness.html',
+      // initialUrlRequest: URLRequest(url: WebUri('https://192.168.31.17:5173/')),
+      initialFile:
+          'packages/identity_verification_flutter_sdk/assets/html/face_liveness.html',
       initialSettings: InAppWebViewSettings(
         mediaPlaybackRequiresUserGesture: false,
         allowsInlineMediaPlayback: true,
